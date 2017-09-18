7c1c65e404d08115691c716bcaa18a86b3b803e1
Robyn Kwok <jingwenkwok@gmail.com>
2017-09-17 20:50:28 -0400

    debug

--- popup.js
+++ popup.js
@@ -1,13 +1,28 @@
 button = document.getElementById('translate');
 
 // automatically translate all highlighted text if on
-Boolean on = false;
-
-button.addEventListener('click', () => {
-  if (on) on = false;
-  else on = true;
-});
+var translateIsOn = false;
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  // toggles whether auto translate is on
+  button.addEventListener('click', () => {
+    if (translateIsOn) {
+      translateIsOn = false;
+    }
+    else {
+      console.log('button clicked');
+      translateIsOn = true;
+    }
+  });
+
+  // grab text to translate
+  document.addEventListener('mouseup', () => {
+    if (translateIsOn && window.getSelection().toString() != "") {
+      textToTranslate = window.getSelection().toString()
+      console.log(textToTranslate);
+    }
+  })
+
 
 });
